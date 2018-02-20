c21052b41ab29bf4fde2d14ccfc6b8557eb16d4b
leonardosibela <leonardo_sibela@hotmail.com>
2018-02-20 23:28:25 +0000

    Adicionando classe de livros

---
+++ loginlivros/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loginlivros
+{
+    class Book
+    {
+        public string name { get; set; }
+        public string author { get; set; }
+        public int year { get; set; }
+
+        public Book(string name, string author, int year)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+        }
+    }
+}
